ac0c2a287ae800165314a33a04deb08b6d4d74b2
Kazuo-Tsubokawa <hiwa1712@gmail.com>
2021-11-15 09:53:19 +0900

    show修正

--- resources/views/songs/show.blade.php
+++ resources/views/songs/show.blade.php
@@ -1,14 +1,17 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <p>{{ $song->title }}</p>
-    <p>{{ $song->artist->name }}</p>
-    <p><a href="{{ route('songs.index') }}">曲送り</a></p>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('title', 'ランダム再生')
+
+@section('content')
+    <table>
+        <tbody>
+            <tr>
+                <td>
+                    <p>{{ $song->title }}</p>
+                    <p>{{ $song->artist->name }}</p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <a href="{{ route('songs.index') }}">曲送り</a>
+@endsection
