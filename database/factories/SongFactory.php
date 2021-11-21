7e890734aac690c7a3ad7ba2cdb38cefc1d711f2
Kazuo-Tsubokawa <hiwa1712@gmail.com>
2021-11-21 10:24:36 +0900

    画像と音源統一

--- database/factories/SongFactory.php
+++ database/factories/SongFactory.php
@@ -19,8 +19,9 @@ class SongFactory extends Factory
             'artist_id' => Artist::inRandomOrder()->first(),
             'category_id' => Category::inRandomOrder()->first(),
             'title' => $this->faker->word(),
-            'file_name' => $this->faker->word() . '.mp3',
+            'file_name' => "lZvHIkqcT24NiLMfsKNx7iEZzf4FqkrY7UVUDdJZ.mp3",
             'description' => $this->faker->paragraph(),
+            'image' => "icmRRWJ7M6wYt3av7CpbjWABDtCiTGlUAGKN0rpF.png"
         ];
     }
 }
